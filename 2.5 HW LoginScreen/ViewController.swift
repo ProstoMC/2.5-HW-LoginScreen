1486eca1a614a3c67895a9898874786087635fb0
ProstoMC <l-63@inbox.ru>
2022-08-09 21:12:33 +0300

    Initial Commit

---
+++ 2.5 HW LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.5 HW LoginScreen
+//
+//  Created by Admin on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
